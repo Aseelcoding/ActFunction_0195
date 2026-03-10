9e1198ff3a9998a254d1c3474e0765758c997af0
Aseelcoding <asilalhashmi@gmail.com>
2026-03-10 10:21:18 +0700

    Add files via upload

---
+++ function_no_parameter.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int length, width;
+
+void input()
+{
+    cout << "Enter Length : ";
+    cin >> length;
+
+    cout << "Enter Width : ";
+    cin >> width;
+}
+
+int rectangleArea()
+{
+    return length * width;
+}
+
+void output()
+{
+    cout << "Result : " << rectangleArea();
+}
+
+int main()
+{
+    input();
+    output();
+}
